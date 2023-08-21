22c9fe63add1fb43436ae457bbde7cfb27780905
Gracdjd <ziwenwang950@gmail.com>
2023-08-21 11:35:00 +0800

    relu初步完成

---
+++ src/relu.c
@@ -0,0 +1,9 @@
+p, l
+if(l < 1) exit
+for(int i = 0; i < l; ++i)
+{
+    if(p[i] < 0)
+    {
+        p[i] = 0;
+    }
+}
--- src/relu.s
+++ src/relu.s
@@ -14,20 +14,27 @@
 # ==============================================================================
 relu:
     # Prologue
+    li t1, 1
+    blt a1, t1, exit_relu
 
-
+    add t0, x0, x0
+    #sll t1, t1, 2
 loop_start:
+    beq t0, a1, loop_end
     
 
-
-
-
-
-
-
 loop_continue:
+    addi t0, x0, 1
+    
+    
+    lw t2, 0(a0)
+    addi a0, x0, 4
+    bgt t2, x0, loop_start
+    sw x0, -4(a0) 
 
-
+exit_relu:
+    li a1, 32
+    j exit2
 
 loop_end:
 
