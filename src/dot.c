0376a2038b478fd3abec7d31ff03bf8eaac67ef1
Gracdjd <ziwenwang950@gmail.com>
2023-08-24 10:57:58 +0800

    finish dot

---
+++ src/dot.c
@@ -0,0 +1,10 @@
+len < 1 || str1 < 1 || str2 < 1 exit
+
+sum = 0
+dot(v1, v2)
+{
+    for(int i = 1; i <= len, ++i)
+    {
+        sum += v1[i + str1] * v2[i + str2] 
+    }
+}
--- src/dot.s
+++ src/dot.s
@@ -18,27 +18,43 @@
 #   this function terminates the program with error code 33
 # =======================================================
 dot:
-
     # Prologue
+    li t0, 1
+    blt a2, t0, exit_32
+    blt a3, t0, exit_33
+    blt a4, t0, exit_33
+    #sum
+    li t1, 0
 
-
+    sll a3, a3, 2
+    sll a3, a3, 2
+    #t0 作为temp
 loop_start:
 
+    bge t0, a2, loop_end
 
+    lw t2, 0(a0)
+    lw t3, 0(a1)
 
+    add a0, a0, a3
+    add a1, a1, a4
 
-
-
-
-
-
-
-
+    mul t4, t2, t3
+    add t1, t1, t4
+    j loop_start
 
 loop_end:
 
-
+    add a0, t1, x0
     # Epilogue
 
     
     ret
+
+exit_32:
+    li a1, 32
+    j exit2
+
+exit_33
+    li a1, 33
+    j exit2
